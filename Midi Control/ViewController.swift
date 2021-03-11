f396d3743b7b514452755f5c0fb86c1f0c05e31a
Morgan Jones <46874806+morgsj@users.noreply.github.com>
2021-03-11 19:02:53 +0000

    Initial Commit

---
+++ Midi Control/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Midi Control
+//
+//  Created by Morgan Jones on 11/03/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
